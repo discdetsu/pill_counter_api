0b82540171ea52ae13191447c8e70ee8f5b8c3e7
discdetsu <disk.nitrome@gmail.com>
2022-09-08 09:54:10 +0700

    add to local again

--- webapp.py
+++ webapp.py
@@ -8,16 +8,6 @@ import warnings
 warnings.filterwarnings("ignore")
 
 model = torch.hub.load("yolov7_local", 'custom', "PillDetectorYOLOv7.pt", source='local', force_reload=True)
-# results = model('1.jpg', size=640)
-# results = results.pandas().xyxy[0].values.tolist()
-
-# img = mpimg.imread('1.jpg')
-# annotations = []
-# for pill in results:
-#     ann = {'class': 'pill', 'bbox': [int(pill[0]), int(pill[1]), int(pill[2]), int(pill[3])]}
-#     annotations.append(ann)
-# print(annotations)
-
 
 app = FastAPI()
 
@@ -30,21 +20,6 @@ favicon_path = 'favicon.ico'
 @app.get('/favicon.ico')
 async def favicon():
     return FileResponse(favicon_path)
-# @app.post("/plot/", responses = {
-#         200: {
-#             "content": {"image/jpg": {}}
-#         }
-#     },
-#     response_class=Response
-# )
-# async def create_file(file: bytes = File()):
-#     img = Image.open(io.BytesIO(file))
-#     results = model(img, size=640)
-#     results.render() 
-#     res = cv2.cvtColor(results.imgs[0], cv2.COLOR_BGR2RGB)
-#     img_encode = cv2.imencode('.jpg', res)[1].tobytes()
-
-#     return Response(content=img_encode, media_type="image/jpg")
 
 @app.post("/image/", response_class=ORJSONResponse)
 async def read_items(file: bytes = File()):
@@ -58,5 +33,3 @@ async def read_items(file: bytes = File()):
         ann = {'class': 'pill', 'bbox': [pill[0], pill[1], pill[2], pill[3]]}
         res['annotations'].append(ann)
     return ORJSONResponse(res)
-
-# pip install orjson
